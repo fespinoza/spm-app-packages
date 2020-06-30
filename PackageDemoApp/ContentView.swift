14d8b7cdd1ab03069f36d7d43809188dc213d9b9
Felipe Espinoza <fespinozacast@gmail.com>
2020-06-30 09:47:50 +0200

    Initial Commit

---
+++ PackageDemoApp/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  PackageDemoApp
+//
+//  Created by Felipe Espinoza on 30/06/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
